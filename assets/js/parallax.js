25ef80ea964dc1877635145bfd666a5a471cd925
delaroche <delarochetheau1409@gmail.com>
2018-12-12 16:27:07 +0100

    create file system aand start parallax

---
+++ assets/js/parallax.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $(window).bind('scroll',function(e){
+      parallaxScroll();
+  });
+
+  function parallaxScroll(){
+    var scrolled = $(window).scrollTop();
+    $('#parallax-bg3').css('top',(0-(scrolled*.7))+'px');
+  }
+})
