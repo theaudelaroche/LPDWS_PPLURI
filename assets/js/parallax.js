464da3b4c93d7266ffddce867e50dce8d2522b41
delaroche <delarochetheau1409@gmail.com>
2018-12-13 13:02:19 +0100

    change gif on section change

--- assets/js/parallax.js
+++ assets/js/parallax.js
@@ -7,29 +7,4 @@ $(document).ready(function(){
     var scrolled = $(window).scrollTop();
     $('#parallax-bg3').css('top',(0-(scrolled*.3))+'px');
   }
-
-  function changeSrc(){
-  	var scrolled = $(window).scrollTop();
-  	var screen = $(window).height();
-
-  	if(scrolled<screen){
-  		console.log('premiere');
-  		$('.gif-1').css("display", "block");
-  		$('.gif-2').css("display", "none");
-  		$('.gif-3').css("display", "none");
-  		// premiere
-  	} else if (scrolled>screen && scrolled<2*screen){
-  		console.log('deuxieme');
-  		$('.gif-1').css("display", "none");
-  		$('.gif-2').css("display", "block");
-  		$('.gif-3').css("display", "none");
-  		// deuxieme section
-  	} else if (scrolled>2*screen){
-  		// troisieme
-  		console.log('troisieme')
-  		$('.gif-1').css("display", "none");
-  		$('.gif-2').css("display", "none");
-  		$('.gif-3').css("display", "block");
-  	}
-  }
 })
